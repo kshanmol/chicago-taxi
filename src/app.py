bd5a7e8ca57a86f6456bc46ec25858754d5bc493
Anmol Singh <kshasingh@gmail.com>
2022-02-16 19:27:16 +0530

    Group data by date, add total_trips and average_speed_24hrs endpoints

--- src/app.py
+++ src/app.py
@@ -1,13 +1,29 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-from helpers import load_parquet
+from helpers import load_parquet, group_by_date
+from helpers import total_trips, average_speed_24hrs
+
+import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
 data = load_parquet("/tmp/chicago_taxi_trips_2020.parquet")
 print(data.info())
+grouped_data = group_by_date(data)
+
+@app.route('/total_trips', methods=['GET'])
+def get_trips():
+    start_date = request.args.get('start')
+    end_date = request.args.get('end')
+    return json.dumps(total_trips(start_date, end_date, grouped_data))
+
+@app.route('/average_speed_24hrs', methods=['GET'])
+def get_average_speed_24hrs():
+    date = request.args.get('date')
+    return json.dumps(average_speed_24hrs(date, grouped_data))
 
 @app.route('/', methods=['GET'])
 def index():
--- src/helpers.py
+++ src/helpers.py
@@ -1,8 +1,64 @@
+import pandas as pd
+import datetime
+
 import pyarrow.parquet as pq
 
 cols = ['unique_key', 'taxi_id', 'trip_start_timestamp', 'trip_end_timestamp', 
 'trip_seconds', 'trip_miles', 'fare', 'trip_total', 'pickup_latitude', 'pickup_longitude']
 
+range_first_date = pd.to_datetime("2020-01-01", format="%Y-%m-%d", yearfirst=True)
+range_last_date = pd.to_datetime("2020-12-31", format="%Y-%m-%d", yearfirst=True)
+
 def load_parquet(path, columns=cols):
     table = pq.read_table(path, columns=columns)
     return table.to_pandas()
+
+def group_by_date(table):
+    groups = {}
+
+    start_date = range_first_date
+    delta = datetime.timedelta(days=1)
+
+    while(start_date <= range_last_date):
+        next_date = start_date + delta
+        mask = (table['trip_start_timestamp'] >= start_date) & (table['trip_start_timestamp'] < next_date)
+        groups[start_date] = table[mask]
+        start_date = next_date
+    
+    return groups
+
+def total_trips(start_date, end_date, groups):
+
+    start_date = pd.to_datetime(start_date, format="%Y-%m-%d", yearfirst=True)
+    end_date = pd.to_datetime(end_date, format="%Y-%m-%d", yearfirst=True)
+
+    if end_date < start_date:
+        return "Error: End date cannot be before start date"
+    if start_date < range_first_date:
+        return "Error: Start not in range"
+    if end_date > range_last_date:
+        return "Error: End date not in range"
+
+    delta = datetime.timedelta(days=1)
+    result = {"data" : []}
+    while(start_date <= end_date):
+        trips = {"date" : start_date.strftime('%Y-%m-%d'), "total_trips": len(groups[start_date].index)}
+        result['data'].append(trips)
+        start_date += delta
+    
+    return result
+
+def average_speed_24hrs(date, groups):
+
+    date = pd.to_datetime(date, format="%Y-%m-%d", yearfirst=True)
+    target_date = date - datetime.timedelta(days=1)
+    if target_date < range_first_date or target_date > range_last_date:
+        return "Error: Date not in range"
+
+    total_km = groups[target_date]['trip_miles'].sum() * 1.60934
+    total_hr = groups[target_date]['trip_seconds'].sum() / (3600.0)
+    average_speed = total_km / total_hr
+
+    result = {"data" : [ {"average_speed" : f'{average_speed:.2f}'} ]}
+    return result
+
