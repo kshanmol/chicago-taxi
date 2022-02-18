9dd55f36a109f0a3b1beb7c969a48dd96e5baf7e
Anmol Singh <kshasingh@gmail.com>
2022-02-18 13:51:38 +0530

    Add average fare heatmap endpoint, set up pytest

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM python:3.7.4
 ADD . /take-home
 WORKDIR /take-home
 RUN pip3 install -r requirements.txt
-RUN cd /tmp && python3 /take-home/downloader.py 1QLBGFOoKw_3-iM58q4unWfwHmPqfnrYr chicago_taxi_trips_2020.parquet
+RUN cd /tmp && wget -O chicago_taxi_trips_2020.parquet "https://www.dropbox.com/s/r71iostpglhrlwg/chicago_taxi_trips_2020.parquet?dl=0"
+#RUN PYTHONPATH=$(pwd)/src/ pytest
--- src/app.py
+++ src/app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-from helpers import load_parquet, group_by_date
-from helpers import total_trips, average_speed_24hrs
+from helpers import load_parquet, group_by_date, get_bounding_box
+from helpers import total_trips, average_speed_24hrs, average_fare_heatmap
 
 import json
 
@@ -13,12 +13,22 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 data = load_parquet("/tmp/chicago_taxi_trips_2020.parquet")
 print(data.info())
 grouped_data = group_by_date(data)
+bounding_box = get_bounding_box(data)
 
 @app.route('/total_trips', methods=['GET'])
 def get_trips():
     start_date = request.args.get('start')
     end_date = request.args.get('end')
     return json.dumps(total_trips(start_date, end_date, grouped_data))
+
+@app.route('/average_fare_heatmap', methods=['GET'])
+def get_average_fare_heatmap():
+    date = request.args.get('date')
+    if 'grid_side_length' in request.args:
+        grid_side_length = int(request.args.get('grid_side_length'))
+        return json.dumps(average_fare_heatmap(date, grouped_data, bounding_box, grid_side_length))
+    else:
+        return json.dumps(average_fare_heatmap(date, grouped_data, bounding_box))
 
 @app.route('/average_speed_24hrs', methods=['GET'])
 def get_average_speed_24hrs():
@@ -27,7 +37,7 @@ def get_average_speed_24hrs():
 
 @app.route('/', methods=['GET'])
 def index():
-    return "Hello, world"
+    return json.dumps("Hello, world")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
